660c6923f2ab18aa6882aabb579405cea6b996c8
Jose Alberto González von Schmeling <josego85@gmail.com>
2020-03-25 17:53:23 -0400

    Mejoras del mapa en el live

--- public/assets/js/MapaOSM.js
+++ public/assets/js/MapaOSM.js
@@ -1,23 +1,22 @@
-var mapa = null;
-
 function cargarMapa()
 {
 	// Asuncion - Paraguay.
+	var mapa = null;
+	var marcador = null;
 	var longitud = -57.6309129;
 	var latitud = -25.2961407;
-	var marcador = null;
-	var zoom = 12;
+	var zoom = 10;
 	var minZoom = 6;
-	var maxZoom = 18;
+	var maxZoom = 15;
 
 	mapa = new L.map('mapa',
     {
         center: [latitud, longitud],
-        minZoom: minZoom,
-		maxZoom: maxZoom,
 		zoom: zoom,
-        scrollWheelZoom: false,
-        fullscreenControl: true,
+		minZoom: minZoom,
+		maxZoom: maxZoom,
+		fullscreenControl: true,
+		scrollWheelZoom: false,
         fullscreenControlOptions:
         {
             position: 'topleft'
@@ -32,27 +31,28 @@ function cargarMapa()
 		attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright">' +
           'OpenStreetMap Contributors </a> Tiles \u00a9 HOT'
 	}).addTo(mapa);
-	
-    marcador = new L.marker([latitud, longitud], {
-		id: 'vendor', 
-	    draggable: 'true'
-	});
-    mapa.addLayer(marcador);
 
-    marcador.on("dragend", function(e) {
-        var marker = e.target;
+	marcador = new L.marker([latitud, longitud], {
+		id: 'ubicacion',
+		draggable: 'true'
+	});
+	mapa.addLayer(marcador);
+	
+	marcador.on("dragend", function(e)
+	{
+		var marker = e.target;
         var position = marker.getLatLng();
         var lat = position.lat;
         var lng = position.lng;
-        var lat_lng = new L.LatLng(lat, lng);
+		var lat_lng = new L.LatLng(lat, lng);
+		
+		marker.setLatLng(lat_lng,
+		{
+			draggable: 'true'
+		});
+		mapa.panTo(lat_lng);
 
-        marker.setLatLng(lat_lng,
-        {
-            draggable: 'true'
-        });
-        mapa.panTo(lat_lng);
-
-        document.getElementById('user_lat').value = lat;
+		document.getElementById('user_lat').value = lat;
         document.getElementById('user_lng').value = lng;
     });
 }
